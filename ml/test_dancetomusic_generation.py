840b24491d35a4e17a1e44f3bd3b07be1dd37ab8
Azeez A <aabdi2015@gmail.com>
2023-12-11 13:21:10 +0100

    updated generation

--- ml/test_dancetomusic_generation.py
+++ ml/test_dancetomusic_generation.py
@@ -27,18 +27,18 @@ codebook_size = encodec_model.quantizer.codebook_size
 encodec_model.to(device)
 sample_rate = 24000
 
-data_dir = "DanceToMusic/data/samples/5sec_min_data"
+data_dir = "/Users/azeez/Documents/pose_estimation/DanceToMusicApp/ml/data/samples/5sec_expando_dnb"
 dataset = DanceToMusic(data_dir, encoder = encodec_model, sample_rate = sample_rate, device=device)
 
 src_pad_idx = 0
 trg_pad_idx = 0
-learned_weights = '/Users/azeez/Documents/pose_estimation/DanceToMusic/weights/5_sec_best_model_weights_loss_6.733452348148122.pth' 
+learned_weights = '/Users/azeez/Documents/pose_estimation/DanceToMusicApp/ml/model_weights/gen_5_sec_dnb__best_model_11258.7658.pt' 
 embed_size = dataset.data['poses'].shape[2] * dataset.data['poses'].shape[3]
-pose_model = Pose2AudioTransformer(codebook_size, src_pad_idx, trg_pad_idx, device=device, num_layers=2, heads = 2, embed_size=embed_size, dropout=0.1)
+pose_model = Pose2AudioTransformer(codebook_size, src_pad_idx, trg_pad_idx, device=device, num_layers=4, heads = 4, embed_size=embed_size, dropout=0.1)
 pose_model.load_state_dict(torch.load(learned_weights, map_location=device))
 pose_model.to(device)
 
 audio_codes, pose, pose_mask, wav, wav_mask, _, _ = dataset[0]
-output = pose_model.generate(pose.unsqueeze(0).to(device), pose_mask.to(device), max_length = audio_codes.size(1), temperature=2)
+output = pose_model.generate(pose.unsqueeze(0).to(device), pose_mask.to(device), max_length = 377, temperature=2)
 
 print(output[0].shape, audio_codes[0].shape)
